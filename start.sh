f433d99270048ed7cf371dc3f6e9ac8fdbfe59ce
OfficialDanav <132914549+OfficialDanav@users.noreply.github.com>
2023-05-09 11:01:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/OfficialDanav/LDOfficialDanav.git /LDOfficialDanav
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /OfficialDanav
 fi
-cd /LazyPrincess
+cd /LDOfficialDanav
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
